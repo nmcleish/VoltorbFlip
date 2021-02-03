43fca4b96025711a9f1bca5c7d8474f5e4a97a5e
Nikole <nmcleish@gatech.edu>
2021-02-03 18:58:34 -0500

    Added game class functionality

--- board.py
+++ board.py
@@ -10,22 +10,25 @@ def get_level_board(level):
         return level_1[randint(0, 4)]
     # TODO Put in correct values for all the levels above 1
     elif level == 2:
-        level_1 = [(3, 1, 6), (0, 3, 6), (5, 0, 6), (2, 2, 6), (4, 1, 6)]
+        level_1 = [(1, 3, 7), (6, 0, 7), (3, 2, 7), (0, 4, 7), (5, 1, 7)]
         return level_1[randint(0, 4)]
     elif level == 3:
-        level_1 = [(3, 1, 6), (0, 3, 6), (5, 0, 6), (2, 2, 6), (4, 1, 6)]
+        level_1 = [(2, 3, 8), (7, 0, 8), (4, 2, 8), (1, 4, 8), (6, 1, 8)]
         return level_1[randint(0, 4)]
     elif level == 4:
-        level_1 = [(3, 1, 6), (0, 3, 6), (5, 0, 6), (2, 2, 6), (4, 1, 6)]
+        level_1 = [(3, 3, 8), (0, 5, 8), (8, 0, 10), (5, 2, 10), (2, 4, 10)]
         return level_1[randint(0, 4)]
     elif level == 5:
-        level_1 = [(3, 1, 6), (0, 3, 6), (5, 0, 6), (2, 2, 6), (4, 1, 6)]
+        level_1 = [(7, 1, 10), (5, 3, 10), (1, 5, 10), (9, 0, 10), (6, 2, 10)]
         return level_1[randint(0, 4)]
     elif level == 6:
-        level_1 = [(3, 1, 6), (0, 3, 6), (5, 0, 6), (2, 2, 6), (4, 1, 6)]
+        level_1 = [(3, 4, 10), (0, 6, 10), (8, 1, 10), (5, 3, 10), (2, 5, 10)]
         return level_1[randint(0, 4)]
     elif level == 7:
-        level_1 = [(3, 1, 6), (0, 3, 6), (5, 0, 6), (2, 2, 6), (4, 1, 6)]
+        level_1 = [(7, 2, 10), (4, 4, 10), (1, 6, 13), (9, 1, 13), (6, 3, 10)]
+        return level_1[randint(0, 4)]
+    elif level == 8:
+        level_1 = [(0, 7, 10), (8, 2, 10), (5, 4, 10), (2, 6, 10), (7, 3, 10)]
         return level_1[randint(0, 4)]
 
 
@@ -137,5 +140,10 @@ class Board:
         else:
             return 1
 
+    # Returns the number of multipliers greater than 1
+    def get_num_multipliers(self):
+        return self.level_board[0] + self.level_board[1]
 
-b = Board(1)
+    # Returns the number of Voltorbs
+    def get_num_voltorbs(self):
+        return self.level_board[2]
---
+++ voltorbflip.py
@@ -0,0 +1,54 @@
+import board
+
+
+class Game:
+    def __init__(self):
+        self.total_coins = 0
+        self.level_coins = 0
+        self.level = 1
+        self.board = board.Board(self.level)
+        self.num_multipliers = self.board.get_num_multipliers()
+        self.num_voltorbs = self.board.get_num_multipliers()
+        self.total_multipliers = 0
+
+    # Checks the space (x,y) for a multiplier or Voltorb and updates game status and coins.
+    def check_space(self, space):
+        result = self.board.check_space(space)
+        status = ""
+        if result > 1:
+            self.num_multipliers -= 1
+
+            if self.level_coins == 0:
+                self.level_coins += result
+            else:
+                self.level_coins *= result
+
+                if self.num_multipliers == 0:
+                    status = "Level Success"
+        elif result == 0:
+            self.total_coins += self.level_coins
+            status = "Game Over"
+        space_result = (result, self.level_coins, self.total_coins, status)
+        return space_result
+
+    # Gets the value in a space
+    def get_space(self, space):
+        return self.board.check_space(space)
+
+    # Makes a new game board
+    def new_board(self):
+        if self.num_multipliers == 0:
+            self.level += 1
+        elif self.total_multipliers < self.num_voltorbs:
+            if self.total_multipliers == 0:
+                self.level = 1
+            if self.total_multipliers < self.level:
+                self.level = self.total_multipliers
+
+        self.board = board.Board(self.level)
+        self.level_coins = 0
+        self.num_multipliers = self.board.get_num_multipliers()
+        self.num_voltorbs = self.board.get_num_voltorbs()
+        self.total_multipliers = 0
+        self.level_coins = 0
+
